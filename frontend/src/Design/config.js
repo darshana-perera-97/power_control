3b393d7fe4cf3f60aacd3972ca631de9e58897ef
Darshana Perera <91814341+darshana-perera-97@users.noreply.github.com>
2025-03-17 11:31:32 +0530

    backend URL changed

--- frontend/src/Design/config.js
+++ frontend/src/Design/config.js
@@ -1,6 +1,6 @@
 const config = {
   //apiUrl: "http://108.181.199.62:3013/",
-  apiUrl: "http://69.197.187.24/:3013/",
+  apiUrl: "http://69.197.187.24:3013/",
   // apiUrl: "http://localhost:3013/",
   //   apiUrl: "http://localhost:3010/",
 };
